63e74ac1487c71cdaaa525b6ded3f4d728f8e6c4
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2020-11-07 16:54:10 -0300

    Config navigation

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { VehiclesComponent } from './views/vehicles/vehicles.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "vehicles",
+    component: VehiclesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
