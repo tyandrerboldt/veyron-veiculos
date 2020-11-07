512bebe18e00169d7bd2e193a38af1d1ac70da3b
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2020-11-07 16:36:00 -0300

    Finish initial structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import { HomeComponent } from './views/home/home.component';
+import { VehiclesComponent } from './views/vehicles/vehicles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    HomeComponent,
+    VehiclesComponent,
   ],
   imports: [
     BrowserModule,
