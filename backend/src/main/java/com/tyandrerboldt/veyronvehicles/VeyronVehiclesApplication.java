479b24414a01ea6b4b4f7a1b0c6b0fd3df750430
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2020-11-09 19:45:59 -0300

    Create backend project

---
+++ backend/src/main/java/com/tyandrerboldt/veyronvehicles/VeyronVehiclesApplication.java
@@ -0,0 +1,13 @@
+package com.tyandrerboldt.veyronvehicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeyronVehiclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeyronVehiclesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tyandrerboldt/veyronvehicles/VeyronVehiclesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyandrerboldt.veyronvehicles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeyronVehiclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
