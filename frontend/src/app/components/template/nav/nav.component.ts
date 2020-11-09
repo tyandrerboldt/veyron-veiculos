bcbd2a17ef151c9cdd9d25ccbcf8fe406a2d62ee
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2020-11-09 12:51:52 -0300

    Finish vehicles list styles

--- frontend/src/app/components/template/nav/nav.component.ts
+++ frontend/src/app/components/template/nav/nav.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  show:boolean
+
+  constructor() { 
+    this.show = true
+  }
 
   ngOnInit(): void {
+  }
+
+  toggle(){
+    this.show = !this.show
   }
 
 }
