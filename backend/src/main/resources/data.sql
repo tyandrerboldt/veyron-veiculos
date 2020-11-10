709b820cccf33e5623e4bcd6d429d5c435ba7a99
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2020-11-10 11:21:08 -0300

    Ready to test mode

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO vehicles
 	(id, model, board, color, year, fuel)
 VALUES
-	(1, 'Fiat Argo 1.0', 'NZP-4326', 'Branco', 2019, 'flex'),
-	(2, 'Ford Ka SE Plus 1.0', 'OXZ-5225', 'Prata', 2020, 'flex'),
-	(3, 'Hyundai Hilux XL 3.0', 'TRW-2156', 'Vermelho', 2020, 'diesel'),
-	(4, 'Renault Kwid Outsider', 'HRF-1875', 'Prata', 2019, 'gasoline');
+	(1, 'Fiat Argo 1.0', 'NZP4326', 'Branco', 2019, 'flex'),
+	(2, 'Ford Ka SE Plus 1.0', 'DFS3434', 'Prata', 2020, 'flex'),
+	(3, 'Hyundai Hilux XL 3.0', 'DLE2323', 'Vermelho', 2020, 'diesel'),
+	(4, 'Renault Kwid Outsider', 'GQS2D23', 'Prata', 2019, 'gasoline');
