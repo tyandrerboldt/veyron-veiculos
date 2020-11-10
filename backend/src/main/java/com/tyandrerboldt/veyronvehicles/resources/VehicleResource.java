bd014e69606c2335123522e360daaf85f3cbe9b6
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2020-11-09 22:29:58 -0300

    Add update and delete on vehicle's resource

--- backend/src/main/java/com/tyandrerboldt/veyronvehicles/resources/VehicleResource.java
+++ backend/src/main/java/com/tyandrerboldt/veyronvehicles/resources/VehicleResource.java
@@ -1,13 +1,19 @@
 package com.tyandrerboldt.veyronvehicles.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tyandrerboldt.veyronvehicles.dtos.VehicleDTO;
 import com.tyandrerboldt.veyronvehicles.services.VehicleService;
@@ -30,5 +36,28 @@ public class VehicleResource {
 		VehicleDTO vehicleDTO = vehicleService.findById(vehicleId);
 		return ResponseEntity.ok().body(vehicleDTO);
 	}
+	
+	@PostMapping
+	public ResponseEntity<VehicleDTO> create(@RequestBody VehicleDTO vehicleDTO){
+		vehicleDTO = vehicleService.create(vehicleDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{productId}")
+				.buildAndExpand(vehicleDTO.getId()).toUri();
+		return ResponseEntity.created(uri).body(vehicleDTO);		
+	}
+	
+	@PutMapping("/{vehicleId}")
+	public ResponseEntity<VehicleDTO> update(@PathVariable Long vehicleId, 
+			@RequestBody VehicleDTO vehicleDTO){
+		vehicleDTO = vehicleService.update(vehicleId, vehicleDTO);
+		return ResponseEntity.ok().body(vehicleDTO);
+	}
+	
+	@DeleteMapping("/{vehicleId}")
+	public ResponseEntity<VehicleDTO> delete(@PathVariable Long vehicleId){
+		vehicleService.delete(vehicleId);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
 	
 }
--- backend/src/main/java/com/tyandrerboldt/veyronvehicles/services/VehicleService.java
+++ backend/src/main/java/com/tyandrerboldt/veyronvehicles/services/VehicleService.java
@@ -3,12 +3,18 @@ package com.tyandrerboldt.veyronvehicles.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.tyandrerboldt.veyronvehicles.dtos.VehicleDTO;
 import com.tyandrerboldt.veyronvehicles.entities.Vehicle;
 import com.tyandrerboldt.veyronvehicles.repositories.VehicleRepository;
+import com.tyandrerboldt.veyronvehicles.services.exceptions.DatabaseException;
 import com.tyandrerboldt.veyronvehicles.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -16,21 +22,61 @@ public class VehicleService {
 
 	@Autowired
 	private VehicleRepository vehicleRepository;
-	
-	public List<VehicleDTO> findAll(){
-		return vehicleRepository.findAll().stream().map(
-				vehicle -> new VehicleDTO(vehicle))
+
+	@Transactional(readOnly = true)
+	public List<VehicleDTO> findAll() {
+		return vehicleRepository.findAll().stream().map(vehicle -> new VehicleDTO(vehicle))
 				.collect(Collectors.toList());
 	}
-	
+
+	@Transactional(readOnly = true)
 	public VehicleDTO findById(Long vehicleId) {
 		Vehicle vehicle = findOrFail(vehicleId);
 		return new VehicleDTO(vehicle);
 	}
+
+	@Transactional
+	public VehicleDTO create(VehicleDTO vehicleDTO) {
+		Vehicle vehicle = new Vehicle();
+		copyDtoToEntity(vehicleDTO, vehicle);
+		vehicle = vehicleRepository.save(vehicle);
+		return new VehicleDTO(vehicle);
+	}
+
+	@Transactional
+	public VehicleDTO update(Long vehicleId, VehicleDTO vehicleDTO) {
+		try {
+			Vehicle vehicle = vehicleRepository.getOne(vehicleId);
+			copyDtoToEntity(vehicleDTO, vehicle);
+			vehicle = vehicleRepository.save(vehicle);
+			return new VehicleDTO(vehicle);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Veículo não encontrado. ID: " + vehicleId);
+		}
+	}
 	
+	public void delete(Long vehicleId) {
+		try {
+			vehicleRepository.deleteById(vehicleId);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Veículo não encontrado. ID:" + vehicleId);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Este produto está relacionado com outros registros.");
+		}
+	}
+
+	@Transactional(readOnly = true)
 	public Vehicle findOrFail(Long vehicleId) {
 		return vehicleRepository.findById(vehicleId)
 				.orElseThrow(() -> new ResourceNotFoundException("Veículo não encontrado!"));
 	}
-	
+
+	private void copyDtoToEntity(VehicleDTO vehicleDTO, Vehicle vehicle) {
+		vehicle.setModel(vehicleDTO.getModel());
+		vehicle.setBoard(vehicleDTO.getBoard());
+		vehicle.setColor(vehicleDTO.getColor());
+		vehicle.setYear(vehicleDTO.getYear());
+		vehicle.setFuel(vehicleDTO.getFuel());
+	}
+
 }
